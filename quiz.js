b8581c3995953bc1c42adc5f262088c2bae7925e
NickGMV <78032114+NickGMV@users.noreply.github.com>
2022-02-09 18:04:54 +0000

    improving q change and check needed next

--- quiz.js
+++ quiz.js
@@ -7,27 +7,34 @@
 
 var document = "quiz.html"
 
-var qs = JSON.parse(questions);
+const qs = JSON.parse(questions);
 
-var check = JSON.parse(questions).Q1.Question;
+//var check = JSON.parse(questions).Q1.Question;
 
 //window.alert(check);
 var quizstate = 0
 var answer = 0 
+var i = 0
+//var question_list = object.keys(qs);
 
+var keys = [];
+for(var k in qs) keys.push(k);
 
+var question = keys[i]
 function startQuiz(){
     
     if(quizstate != 1){
+        document.getElementById("choices").innerHTML = '';
         window.alert(quizstate);
         document.getElementById("quizover").style.display = "none";
          quizstate = 1;
         //window.alert(gamestate);
-        generatequestion();    
+        generatequestion(question);    
     }
     else{
         quizover();
-        quizstate = 0;}
+        quizstate = 0;
+    }
 }
 
 function quizover(){
@@ -35,21 +42,23 @@ function quizover(){
 
 }
 
-function generatequestion(){
+function generatequestion(question){
     //window.alert('question being generated')
-    var question = qs.Q1.Question; 
-    document.getElementById('question').innerHTML = question
+    //var incorrect = 0;
+    var question1 = eval("qs."+ question);
+    var question2 = question1.Question; 
+    document.getElementById('question').innerHTML = question2
     //console.log(question)
-    var answer = qs.Q1.Correctanswer
+    var answer = question1.Correctanswer
     //console.log(answer)
-    var incorrect = qs.Q1.incorrect
+    var incorrect = question1.incorrect.map((x)=>x)
     console.log(incorrect)
     incorrect.push(answer);
     var answers = incorrect;
-    const answers2 = answers.sort((a, b) => 0.5 - Math.random());
+    var answers2 = answers.sort((a, b) => 0.5 - Math.random());
     console.log(answers2);
 
-    for(var i = 0; i < answers.length-1; i++) {
+    for(var i = 0; i < answers.length; i++) {
         //window.alert(answers[i]);
         gen_box(answers[i],i,'choices');}
         
@@ -58,67 +67,9 @@ function generatequestion(){
 
 function gen_box(x,i,y){
     //window.alert('box being generated')
-    document.getElementById(y).innerHTML+='<div class="box" id="box'+ i + '"onclick="checkAnswer(' + i + ')">'+ x +'</div>'
+    document.getElementById(y).innerHTML+='<div class="box" id="box'+ i + '"onclick="checkAnswer(' + i + ')"><p class="limit">'+ x +'</p></div>'
 }
 
-function questionShow() {
-   
-    var answers = [answer0,answer1,answer2,answer3];
-    //randomise order of answers to be placed in to boxes by for loop
-    var answers2 = randomiseArray(answers);
-    //window.alert(answers2);
-    //place answers in to boxes
-    for(i=0;i<5;i++){document.getElementById("box"+i).innerHTML = answers2[i];
-                     
-    }
-}
-
-function gameOver(){
-    //window.alert("gameover");
-    stopTimer();
-    document.getElementById("gameover").style.display = "block";
-}
-//function to randomise an array 
-function randomiseArray(array) {
-    var array2 = [];
-    
-    while(array.length>0){
-        var i = Math.floor(Math.random()*array.length);
-        array2.unshift(array[i]);
-        array.splice(i,1);
-        }
-    return array2;
-}
-
-      
-function setTimer(){
-   timer = window.setInterval(function(){myTimer()}, 1000); 
-   document.getElementById("timeremaining").style.display = "block";
-}       
-
-        
-function myTimer(){
-    if(time>0){
-    time--;
-    document.getElementById("timeRemaining").innerHTML = time;
-    }
-    else {gameOver();}
-}
- 
-function stopTimer(){
-   // window.alert("timer stopped")
-    clearInterval(timer);
-    time = timesetting;
-    document.getElementById("timeRemaining").innerHTML = time;
-}
-
-
-var score = 0;
-var time = 10;
-// having a default time will allow resetting of difficulty if i want to change it
-var timesetting = 3;
-var timer = ''; 
-var gamestate = 0;
 
 function checkAnswer(b){
     //window.alert(a)
@@ -146,66 +97,7 @@ function updateScore() {
     document.getElementById("gamescore").innerHTML = score;
     //window.alert(score);
 }
-
-
-function startGame(){
-    
-    if(gamestate != 1){
-        //window.alert(gamestate);
-        document.getElementById("gameover").style.display = "none";
-         gamestate=1;
-        //window.alert(gamestate);
-        questionShow();
-       
-        
-    }
-    else{
-        gameOver();
-        gamestate=0;}
-}
-function questionShow() {
-    stopTimer();
-    setTimer();
-    //generating two random digits 1-10
-    var numberOne = Math.floor(Math.random()*10)+1;
-    var numberTwo = Math.floor(Math.random()*10)+1;
-    //write out quesiton in the window
-    document.getElementById("question").innerHTML = numberOne + " &times; " + numberTwo;
-    //create answer and dupes
-    var answer0 = numberOne*numberTwo;
-    a = answer0;
-    var answer1 = answer0 - 1;
-    var answer2 = answer0 + (Math.floor(Math.random()*2)+1)
-    var answer3 = answer0 + (Math.floor(Math.random()*3)+5)
-    //put answers in to array to be randomised
-    var answers = [answer0,answer1,answer2,answer3];
-    //randomise order of answers to be placed in to boxes by for loop
-    var answers2 = randomiseArray(answers);
-    //window.alert(answers2);
-    //place answers in to boxes
-    for(i=0;i<5;i++){document.getElementById("box"+i).innerHTML = answers2[i];
-                     
-    }
-}
-
-function gameOver(){
-    //window.alert("gameover");
-    stopTimer();
-    document.getElementById("gameover").style.display = "block";
-}
-//function to randomise an array 
-function randomiseArray(array) {
-    var array2 = [];
-    
-    while(array.length>0){
-        var i = Math.floor(Math.random()*array.length);
-        array2.unshift(array[i]);
-        array.splice(i,1);
-        }
-    return array2;
-}
-
-      
+     
 function setTimer(){
    timer = window.setInterval(function(){myTimer()}, 1000); 
    document.getElementById("timeremaining").style.display = "block";
